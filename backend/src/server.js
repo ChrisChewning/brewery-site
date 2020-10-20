795f639ed9aaf98748ca9744c1935b1e1fffc12e
ChrisChewning <chewning.cl@gmail.com>
2020-10-19 21:43:06 -0500

    backend endpoints added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,40 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-const app = express();  //app object
+//name fields of community posts.
+const postDetails = {
+  'brewery-meetup': {
+    upvotes: 0,
+    comments: [],
+  },
+  'brewery-tour': {
+    upvotes: 0,
+    comments: [],
+  },
+}
 
-app.use(bodyParser.json()); //parses json object and adds a body property to the req parameter.
 
-app.get('/hello', (req, res) => res.send('Hello'));
-app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
-app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!!`));
+const app = express();
+app.use(bodyParser.json());
+
+//UPVOTE ENDPOINT
+app.post('/api/community/:name/upvotes', (req, res) => {
+  const postName = req.params.name;
+  postDetails[postName].upvotes += 1
+res.status(200).send(`${postName} now has ${postDetails[postName].upvotes} upvotes!`)
+})
+
+//COMMENT ENDPOINT
+app.post('/api/community/:name/add-comment', (req, res) => {
+  const { username, text } = req.body;
+  const postName = req.params.name;
+
+  //add data from body to the fake db.
+  postDetails[postName].comments.push({ username, text });  //push a new object w the username and text from the request body.
+  res.status(200).send(postDetails[postName]);
+
+})
+
 
 app.listen(8000, () => console.log('Listening on port 8000'));  //argument to specify which port to listen on. then a callback
 //that gets called once it connects.
