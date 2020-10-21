580c04ad2d8ea2659a98bcaa8331b28c4366daf9
ChrisChewning <chewning.cl@gmail.com>
2020-10-20 22:33:29 -0500

    mongo to express connection & get route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-//name fields of community posts.
-const postDetails = {
-  'brewery-meetup': {
-    upvotes: 0,
-    comments: [],
-  },
-  'brewery-tour': {
-    upvotes: 0,
-    comments: [],
-  },
-}
-
+import { MongoClient } from 'mongodb'; //can connect to local db
 
 const app = express();
 app.use(bodyParser.json());
+
+//GET COMMENTS AND UPVOTES
+app.get('/api/community/:name', async (req, res) => {
+  try {
+  const postName = req.params.name; //postName gets name from url paramters
+
+  const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+  const db = client.db('brewery-db');
+
+  const postInfo = await db.collection('community').findOne({name: postName})  //matches name url paramter we passed. Mongo returns client object we can use to send queries to the db
+  res.status(200).json(postInfo); //instead of send, use json since we are working with json.
+
+  client.close();
+} catch (error) {
+  res.status(500).json({ message: 'Error connecting to db', error })
+}
+})
 
 //UPVOTE ENDPOINT
 app.post('/api/community/:name/upvotes', (req, res) => {
