01210ebacaf225a6a6312df3b18310a06e6096fa
ChrisChewning <chewning.cl@gmail.com>
2021-02-07 23:35:54 -0600

    progress for geojson

--- myblog/src/components/BreweryIndex.js
+++ myblog/src/components/BreweryIndex.js
@@ -12,27 +12,42 @@ class BreweryIndex extends Component {
     this.state = {
       name: props.match.params.name,
       beers: "",
-      location: "",
+      coordinates: [], //data was in array. need to use [] instead of ""
       image_map: "",
       address: "",
       website: "",
       from_website: "",
       hours: "",
       content: "",
-      image: ""
+      image: "",
+      coords: "",
+      closestThree: [],
     };
   }
 
   componentDidMount() {
-    axios.get(`http://localhost:8000/api/brewery/breweries/${this.state.name}`).then((res) => {
-      const brewery = res.data;
- this.setState({ beers: brewery.beers, location: brewery.location, image_map: brewery.image_map, address: brewery.address, website: brewery.website, from_website: brewery.from_website, hours: brewery.hours, content: brewery.content, image: brewery.image });
-      })
-    };
+    axios.get(`http://localhost:8000/api/brewery/breweries/${this.state.name}`)
+    .then(res => res.data)
+      .then(brewery => {
+        this.setState({ beers: brewery.beers, coordinates: brewery.location.coordinates, image_map: brewery.image_map, address: brewery.address, website: brewery.website, from_website: brewery.from_website, hours: brewery.hours, content: brewery.content, image: brewery.image
+})
+  return axios.get(`http://localhost:8000/api/brewery/${this.state.name}/distance?lat=${brewery.location.coordinates[1]}&lng=${brewery.location.coordinates[0]}`)
+})
+  .then((res) => {
+    const closestThree = res.data.breweries.slice(0,3).map((closest, key) => {
+      this.setState({closestThree: closest.name})
+      console.log(closest.name, 'closest name')
+    })
+  console.log(res.data.breweries.name)
+})
+}
+
+
 
 
 render() {
-console.log(this.state.image)
+console.log(this.state.closestThree)
+
   return (
       <>
       <div className="brewery-index-parent">
@@ -62,6 +77,8 @@ console.log(this.state.image)
         <p className="brewery-index-address">{this.state.address}</p>
       <p className="closest-breweries-header">Make a day of it</p>
       <p className="closest-breweries-subheader">Three closest breweries:</p>
+
+
       </Card>
       </div>
       </>
